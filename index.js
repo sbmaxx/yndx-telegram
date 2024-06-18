bbee8e7107c6ebc6db58dca001117f6ed6626077
sbmaxx <sbmaxx@gmail.com>
2024-06-18 18:28:43 +0300

    wazzup fixed && yahoo finance support

--- index.js
+++ index.js
@@ -65,6 +65,50 @@ bot.command('img', async ctx => {
             source: './example.png'
         });
     } catch (e) {
+        console.error(e);
+        return ctx.replyWithMarkdown('Бот в *отпуске*\n' + e);
+    }
+});
+
+bot.command('premium', async ctx => {
+    console.log(ctx.update.message.from, ctx.update.message.chat);
+
+    const base = 'https://sbmaxx.github.io/yndx/premium.html';
+    const args = [];
+
+    args.push('r=' + Math.random());
+
+    if (CNYACL.includes(ctx.update.message.from.id)) {
+        args.push('cny=1');
+    }
+
+    if (ctx.update.message.from.is_premium) {
+        args.push('premium=1');
+    }
+
+    const url = base + (args.length ? '?' + args.join('&') : '');
+
+    try {
+        const page = await browser.newPage();
+
+        await page.setViewport({
+            width: 800,
+            height: 600,
+            deviceScaleFactor: 1,
+        });
+
+        await page.goto(url);
+
+        await page.waitForSelector('body.loaded');
+
+        await page.waitForTimeout(1500);
+
+        await page.screenshot({ path: 'example.png' });
+
+        return ctx.replyWithPhoto({
+            source: './example.png'
+        });
+    } catch (e) {
     	console.error(e);
         return ctx.replyWithMarkdown('Бот в *отпуске*\n' + e);
     }
@@ -73,9 +117,21 @@ bot.command('img', async ctx => {
 bot.command('wazzup', ctx => {
     console.log(ctx.update.message.from, ctx.update.message.chat);
 
-    fetch('https://rozhdestvenskiy.ru/yndx-stock-server').then(r => r.json()).then(data => {
-        return ctx.replyWithMarkdownV2(processData(data, ctx.update.message.from.id));
-    });
+    const tickers = ['yndx', 'usd', 'eur', 'cny'];
+
+    fetch('https://rozhdestvenskiy.ru/yndx-stock-server')
+        .then(r => r.json())
+        .then(r => {
+            return r.quoteResponse.result
+                .filter((_, i) => i > 0 && i <= 4)
+                .map((o, i) => ({
+                    fallback: o.marketPrice,
+                    price: o.regularMarketPrice,
+                    change: o.regularMarketChange,
+                    ticker: tickers[i]
+                }));
+        })
+        .then(data => ctx.replyWithMarkdownV2(processData(data, ctx.update.message.from.id)));
 });
 
 if (process.env.NODE_ENV === 'production') {
@@ -107,40 +163,24 @@ process.on('SIGTERM', async () => {
     process.exit(0);
 });
 
-function processData(data, userId) {
-    const addContent = ({ change, price, ticker }) => {
-        const strChange = change.toFixed(2);
+function processData(stocks, userId) {
+    const addContent = ({ change, price, ticker, fallback }) => {
+        const strPrice = (price || fallback).toFixed(2);
+        const strChange = (change || 0).toFixed(2);
         const postfix = (change > 0 && strChange !== '0.00' ? '+' : '') + strChange.replace('-', '−');
 
-        return `*${ticker}* \n||${price.replace('.', '\\.')}, ${postfix.replace('.', '\\.').replace('+', '\\+')}||\n`;
+        return `*${ticker}* ${strPrice.replace('.', '\\.')}, ${postfix.replace('.', '\\.').replace('+', '\\+')}`;
     }
 
-    const moex = data.quoteResponse.result[1];
-    const forexUSD = data.quoteResponse.result[2];
-    const forexEUR = data.quoteResponse.result[3];
-    const forexCNY = data.quoteResponse.result[4];
-
-    const moexPrice = moex.regularMarketPrice.toFixed(2);
-    const moexChange = moex.regularMarketChange;
-
-    const forexUSDPrice = forexUSD.regularMarketPrice.toFixed(2);
-    const forexUSDChange = forexUSD.regularMarketChange;
-
-    const forexEURPrice = forexEUR.regularMarketPrice.toFixed(2);
-    const forexEURChange = forexEUR.regularMarketChange;
-
-    const forexCNYPrice = forexCNY.regularMarketPrice.toFixed(2);
-    const forexCNYChange = forexCNY.regularMarketChange;
-
-    let response = '';
-
-    response += addContent({ ticker: 'YNDX', change: moexChange, price: moexPrice });
-    response += addContent({ ticker: 'USD', change: forexUSDChange, price: forexUSDPrice });
-    response += addContent({ ticker: 'EUR', change: forexEURChange, price: forexEURPrice });
-
-    if (CNYACL.includes(userId)) {
-        response += addContent({ ticker: 'CNY', change: forexCNYChange, price: forexCNYPrice });
-    }
-
-    return response;
+    return stocks.filter(({ ticker }) => {
+        if (ticker === 'cny' && CNYACL.includes(userId)) {
+            return true;
+        }
+        return true;
+    }).map(({ change, price, fallback, ticker }) => addContent({
+        change,
+        price,
+        fallback,
+        ticker
+    })).join('\n');
 }
